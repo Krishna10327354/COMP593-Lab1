1788d653a7d2c823948519db0a689789d80134cf
krishna <krishna@flemingcollege.ca>
2024-05-29 16:00:27 -0400

    modified both files in the  repo  clone
    -deleted a  print() statement from script1.py
    -added two print() statements to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 #print lines from zen  of python
 print("Special cases aren't special enough to break the rules.")
-print("In the face of ambiguity, refuse the temptation to guess.")
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 #print lines from zen  of python
 print("Beautiful is better than ugly.")
 print("Complex is better than complicated.")
+print("Namespace are one honking great idea -- let's do more of those!")
